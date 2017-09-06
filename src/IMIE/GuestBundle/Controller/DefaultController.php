5f9a686629e8783f90a865a03164f3df19a20195
BenBenDev <benben.dev@gmail.com>
2017-09-06 16:50:12 +0200

    [routing] Adding routes  /formations  /formation/{id}   /tutoriel/{id}

--- src/IMIE/GuestBundle/Controller/DefaultController.php
+++ src/IMIE/GuestBundle/Controller/DefaultController.php
@@ -16,6 +16,14 @@ class DefaultController extends Controller
     }
     
     /**
+     * @Route("/tutoriel/{id}", name="detail_tutoriel")
+     */
+    public function tutorielAction($id)
+    {
+        return $this->render('IMIEGuestBundle:Tutoriels:detail.html.twig');
+    }
+    
+    /**
      * @Route("/tutoriels", name="liste_tutoriels")
      */
     public function liste_tutorielsAction()
@@ -23,5 +31,20 @@ class DefaultController extends Controller
         return $this->render('IMIEGuestBundle:Tutoriels:index.html.twig');
     }
     
+    /**
+     * @Route("/formation/{id}", name="liste_formations", requirements={"id": "\d+"})
+     */
+    public function formationAction($id)
+    {
+        return $this->render('IMIEGuestBundle:Products:detail.html.twig');
+    }
+    
+    /**
+     * @Route("/formations", name="detail_formation")
+     */
+    public function liste_formationsAction()
+    {
+        return $this->render('IMIEGuestBundle:Products:index.html.twig');
+    }
     
 }
