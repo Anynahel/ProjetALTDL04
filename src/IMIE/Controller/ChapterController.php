1b4c6831e7600176dd0ec7bc94c36f1b636d985b
BenBenDev <benben.dev@gmail.com>
2017-09-08 17:02:45 +0200

    [merge]

--- src/IMIE/Controller/ChapterController.php
+++ src/IMIE/Controller/ChapterController.php
@@ -5,7 +5,8 @@ namespace IMIE\Controller;
 use IMIE\Entity\Chapter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Chapter controller.
--- src/IMIE/Entity/Document.php
+++ src/IMIE/Entity/Document.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 namespace IMIE\Entity;
